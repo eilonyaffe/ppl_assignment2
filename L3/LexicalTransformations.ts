9002576e6a067ccce59ac6a7f37f96e9f77b10c6
eliyaya <157639716+ely142@users.noreply.github.com>
2024-06-27 01:34:08 +0300

    Update LexicalTransformations.ts

--- L3/LexicalTransformations.ts
+++ L3/LexicalTransformations.ts
@@ -1,20 +1,18 @@
-import { ClassExp, ProcExp, Exp, Program, makeProcExp, makePrimOp, makeVarDecl, makeIfExp, makeLitExp, IfExp, isExp, isProgram, makeProgram, isAtomicExp, makeLetExp, isClassExp } from "./L3-ast";
-//import { Result, makeFailure } from "../shared/result";
-import { VarDecl, CExp, Binding, makeAppExp, makeVarRef, makeBoolExp, makeStrExp, isIfExp, isAppExp, isProcExp, isLetExp, isCExp, isDefineExp, makeDefineExp} from "./L3-ast"; //new
+import { ClassExp, ProcExp, Exp, Program, makeProcExp, makePrimOp, makeVarDecl, makeIfExp, makeLitExp, IfExp, isExp, isProgram, makeProgram, isAtomicExp, isClassExp } from "./L3-ast";
+import { VarDecl, CExp, Binding, makeAppExp, makeVarRef, makeBoolExp, isIfExp, isAppExp, isProcExp, isCExp, isDefineExp, makeDefineExp} from "./L3-ast"; 
 import { makeSymbolSExp } from "./L3-value";
-import { Result, makeOk, makeFailure, bind, mapResult, mapv } from "../shared/result"; //new
-import { map, zipWith } from "ramda";
+import { Result, makeOk } from "../shared/result"; 
+import { map } from "ramda";
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: class2proc(classExp)
 Type: ClassExp => ProcExp
 */
 
-export const makeNestedifs = (methods: Binding[]): IfExp => {
-    const curr: Binding = methods[0];
-    return (methods.length == 1 ? makeIfExp(makeAppExp(makePrimOp("eq?"), [makeVarRef("msg"), makeLitExp(makeSymbolSExp(curr.var.var))]), makeAppExp(curr.val, []), makeBoolExp(false)):
-    makeIfExp(makeAppExp(makePrimOp("eq?"), [makeVarRef("msg"), makeLitExp(makeSymbolSExp(curr.var.var))]), makeAppExp(curr.val, []), makeNestedifs(methods.slice(1))))
-}
+export const makeNestedifs = (methods: Binding[]): IfExp => 
+    methods.length == 1 ? makeIfExp(makeAppExp(makePrimOp("eq?"), [makeVarRef("msg"), makeLitExp(makeSymbolSExp(methods[0].var.var))]), makeAppExp(methods[0].val, []), makeBoolExp(false)):
+    makeIfExp(makeAppExp(makePrimOp("eq?"), [makeVarRef("msg"), makeLitExp(makeSymbolSExp(methods[0].var.var))]), makeAppExp(methods[0].val, []), makeNestedifs(methods.slice(1)))
+
 
 export const class2proc = (exp: ClassExp): ProcExp => {
     const args: VarDecl[] = exp.fields;
