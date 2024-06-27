88e5670deb625b60aca8072b9a460323e79caf87
eliyaya <157639716+ely142@users.noreply.github.com>
2024-06-27 21:32:01 +0300

    Update L3-env-sub.ts

--- L3/L3-env-sub.ts
+++ L3/L3-env-sub.ts
@@ -1,143 +1,24 @@
-// L3-eval.ts
-import { map, filter, zipWith } from "ramda";
-import { isCExp, isLetExp } from "./L3-ast";
-import { BoolExp, CExp, Exp, IfExp, LitExp, NumExp,
-         PrimOp, ProcExp, Program, StrExp, VarDecl, ClassExp, Binding } from "./L3-ast";
-import { isAppExp, isBoolExp, isDefineExp, isIfExp, isLitExp, isNumExp,
-             isPrimOp, isProcExp, isStrExp, isVarRef, isClassExp } from "./L3-ast";
-import { makeBoolExp, makeLitExp, makeNumExp, makeProcExp, makeStrExp, makeClassExp,makeBinding } from "./L3-ast";
-import { parseL3Exp } from "./L3-ast";
-import { applyEnv, makeEmptyEnv, makeEnv, Env } from "./L3-env-sub";
-import { isClosure, makeClosure, Closure, Value, isClassSExp, makeClassSExp, ClassSExp, makeObject, Object, isObject, isSymbolSExp } from "./L3-value";
-import { first, rest, isEmpty, List, isNonEmptyList } from '../shared/list';
-import { isBoolean, isNumber, isString } from "../shared/type-predicates";
-import { Result, makeOk, makeFailure, bind, mapResult} from "../shared/result";
-import { renameExps, substitute } from "./substitute";
-import { applyPrimitive } from "./evalPrimitive";
-import { parse as p } from "../shared/parser";
-import { Sexp } from "s-expression";
-import { format } from "../shared/format";
-
 // ========================================================
-// Eval functions
+// Environment data type for L3
+import { Value } from './L3-value';
+import { Result, makeFailure, makeOk } from '../shared/result';
 
-const L3applicativeEval = (exp: CExp, env: Env): Result<Value> =>
-    isNumExp(exp) ? makeOk(exp.val) : 
-    isBoolExp(exp) ? makeOk(exp.val) :
-    isStrExp(exp) ? makeOk(exp.val) :
-    isPrimOp(exp) ? makeOk(exp) :
-    isVarRef(exp) ? applyEnv(env, exp.var) :
-    isLitExp(exp) ? makeOk(exp.val) :
-    isIfExp(exp) ? evalIf(exp, env) :
-    isProcExp(exp) ? evalProc(exp, env) :
-    isAppExp(exp) ? bind(L3applicativeEval(exp.rator, env), (rator: Value) =>
-                        bind(mapResult(param => 
-                            L3applicativeEval(param, env), 
-                              exp.rands), 
-                            (rands: Value[]) =>
-                                L3applyProcedure(rator, rands, env))) :
-    isLetExp(exp) ? makeFailure('"let" not supported (yet)') :
-    isClassExp(exp) ? evalClassExp(exp, env) :
-    makeFailure('Never');
-
-export const isTrueValue = (x: Value): boolean =>
-    ! (x === false);
-
-const evalIf = (exp: IfExp, env: Env): Result<Value> =>
-    bind(L3applicativeEval(exp.test, env), (test: Value) => 
-        isTrueValue(test) ? L3applicativeEval(exp.then, env) : 
-        L3applicativeEval(exp.alt, env));
-
-const evalProc = (exp: ProcExp, env: Env): Result<Closure> =>
-    makeOk(makeClosure(exp.args, exp.body));
-
-    
-const evalClassExp = (exp: ClassExp, env: Env): Result<ClassSExp> =>
-    makeOk(makeClassSExp(exp.fields, exp.methods));
-
-const L3applyProcedure = (proc: Value, args: Value[], env: Env): Result<Value> =>
-    isPrimOp(proc) ? applyPrimitive(proc, args) :
-    isClosure(proc) ? applyClosure(proc, args, env) :
-    isClassSExp(proc) ? applyClassSExp(proc, args, env) :
-    isObject(proc) ? applyObject(proc, args, env) :
-    makeFailure(`Bad procedure ${format(proc)}`);
-
-// Applications are computed by substituting computed
-// values into the body of the closure.
-// To make the types fit - computed values of params must be
-// turned back in Literal Expressions that eval to the computed value.
-const valueToLitExp = (v: Value): NumExp | BoolExp | StrExp | LitExp | PrimOp | ProcExp | ClassExp =>
-    isNumber(v) ? makeNumExp(v) :
-    isBoolean(v) ? makeBoolExp(v) :
-    isString(v) ? makeStrExp(v) :
-    isPrimOp(v) ? v :
-    isClosure(v) ? makeProcExp(v.params, v.body) :
-    isClassSExp(v) ? makeClassExp(v.fields, v.methods) :
-    makeLitExp(v);
-
-const applyClosure = (proc: Closure, args: Value[], env: Env): Result<Value> => {
-    const vars = map((v: VarDecl) => v.var, proc.params);
-    const body = renameExps(proc.body);
-    const litArgs : CExp[] = map(valueToLitExp, args);
-    return evalSequence(substitute(body, vars, litArgs), env);
-    //return evalSequence(substitute(proc.body, vars, litArgs), env);
+export type Env = EmptyEnv | NonEmptyEnv;
+export type EmptyEnv = {tag: "EmptyEnv" }
+export type NonEmptyEnv = {
+    tag: "Env";
+    var: string;
+    val: Value;
+    nextEnv: Env;
 }
+export const makeEmptyEnv = (): EmptyEnv => ({tag: "EmptyEnv"});
+export const makeEnv = (v: string, val: Value, env: Env): NonEmptyEnv =>
+    ({tag: "Env", var: v, val: val, nextEnv: env});
+export const isEmptyEnv = (x: any): x is EmptyEnv => x.tag === "EmptyEnv";
+export const isNonEmptyEnv = (x: any): x is NonEmptyEnv => x.tag === "Env";
+export const isEnv = (x: any): x is Env => isEmptyEnv(x) || isNonEmptyEnv(x);
 
-const applyClassSExp = (proc: ClassSExp, args: Value[], env: Env): Result<Value> =>{ 
-    const afterSub = substitute(map((b:Binding)=>b.val, proc.methods), map((v:VarDecl)=>v.var, proc.fields), map((v:Value)=>valueToLitExp(v), args)); //we substitute those parameters into the methods
-    const bindingsWParams = zipWith(makeBinding, map((b:Binding)=>b.var.var, proc.methods), afterSub); //we get new Binding[] with substituted parameters
-    const newClSExp = makeClassSExp(proc.fields,bindingsWParams);
-    return makeOk(makeObject(newClSExp));
-}
-
-const applyObject = (proc: Object, args: Value[], env: Env): Result<Value> => {
-    if(!isEmpty(args) && isSymbolSExp(args[0])){
-        const methodName = args[0].val;
-        const correctMethod = filter((b:Binding)=>b.var.var==methodName, proc.classType.methods);
-        if(!isEmpty(correctMethod)){
-            if(isProcExp(correctMethod[0].val))
-                return applyClosure(makeClosure(correctMethod[0].val.args, correctMethod[0].val.body), args.slice(1), env);
-            else{
-                return makeFailure(`Unrecognized method: ${methodName}`);
-            }
-        }
-        else{
-            return makeFailure(`Unrecognized method: ${methodName}`);
-        }
-    }
-    else{
-        return makeFailure(`Bad object call`)
-    }
-}
-
-// Evaluate a sequence of expressions (in a program)
-export const evalSequence = (seq: List<Exp>, env: Env): Result<Value> =>
-    isNonEmptyList<Exp>(seq) ? 
-        isDefineExp(first(seq)) ? evalDefineExps(first(seq), rest(seq), env) :
-        evalCExps(first(seq), rest(seq), env) :
-    makeFailure("Empty sequence");
-
-const evalCExps = (first: Exp, rest: Exp[], env: Env): Result<Value> =>
-    isCExp(first) && isEmpty(rest) ? L3applicativeEval(first, env) :
-    isCExp(first) ? bind(L3applicativeEval(first, env), _ => 
-                            evalSequence(rest, env)) :
-    makeFailure("Never");
-
-// Eval a sequence of expressions when the first exp is a Define.
-// Compute the rhs of the define, extend the env with the new binding
-// then compute the rest of the exps in the new env.
-const evalDefineExps = (def: Exp, exps: Exp[], env: Env): Result<Value> =>
-    isDefineExp(def) ? bind(L3applicativeEval(def.val, env), 
-                            (rhs: Value) => 
-                                evalSequence(exps, 
-                                    makeEnv(def.var.var, rhs, env))) :
-    makeFailure(`Unexpected in evalDefine: ${format(def)}`);
-
-// Main program
-export const evalL3program = (program: Program): Result<Value> =>
-    evalSequence(program.exps, makeEmptyEnv());
-
-export const evalParse = (s: string): Result<Value> =>
-    bind(p(s), (sexp: Sexp) => 
-        bind(parseL3Exp(sexp), (exp: Exp) =>
-            evalSequence([exp], makeEmptyEnv())));
+export const applyEnv = (env: Env, v: string): Result<Value> =>
+    isEmptyEnv(env) ? makeFailure(`var not found: ${v}`) :
+    env.var === v ? makeOk(env.val) :
+    applyEnv(env.nextEnv, v);
